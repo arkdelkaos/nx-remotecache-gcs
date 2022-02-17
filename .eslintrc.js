135db9a25817afab15124bb2996d1134a25e9832
Alvaro Almendros Gala <alvaro.almendros@masmovil.com>
2022-02-17 14:04:18 +0100

    fix tsc error

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    'prettier/prettier': 'warn',
+    'prettier/prettier': ['warn', { usePrettierrc: true }],
     'require-jsdoc': 'off',
   },
 };
